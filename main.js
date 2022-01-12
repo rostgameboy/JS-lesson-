5cce5c0db9c6d57cadedfa5e3f018da55e6a0509
rostgameboy <92036191+rostgameboy@users.noreply.github.com>
2022-01-12 18:05:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Task1")
+a = 23
+P = 4 * a
+console.log(P)
+
+console.log("Task2")
+a = 24
+S = a**2
+console.log(S)
+
+console.log("Task3")
+a = 25
+b = 30
+S = a * b
+P = 2 * (a + b)
+console.log(S)
+console.log(P)
+
+console.log("Task4")
+d = 20
+L = Math.PI * d
+console.log(L)
