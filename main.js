f5175fd1c843c015a564b40817c27bfe3281e2a9
rostgameboy <92036191+rostgameboy@users.noreply.github.com>
2022-01-14 14:56:20 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,19 @@ console.log("Task4")
 d = 20
 L = Math.PI * d
 console.log(L)
+
+console.log("task5")
+a = 20
+V = a**3
+S = 6 * a**2
+console.log(V)
+console.log(S)
+
+console.log("Task6")
+a = 15
+b = 13
+c = 20
+V = a * b * c
+S = 2 * (a * b + b * c + a * c)
+console.log(V)
+console.log(S)
