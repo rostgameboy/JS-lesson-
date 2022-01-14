b54313cc87a460543f326ce3726cb3733b12f769
rostgameboy <92036191+rostgameboy@users.noreply.github.com>
2022-01-14 15:31:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -36,3 +36,34 @@ V = a * b * c
 S = 2 * (a * b + b * c + a * c)
 console.log(V)
 console.log(S)
+
+console.log("Task7")
+R = 20
+L = 2 * 3.14 * R
+S = 3.14 * R**2
+console.log(L)
+console.log(S)
+
+console.log("Task8")
+a = 55
+b = 83
+SA = (a + b) / 2
+console.log(SA)
+
+console.log("Task9")
+a = 70
+b = 37
+SG = Math.sqrt(a * b)
+console.log(SG)
+
+console.log("Task10")
+a = 24**2
+b = 70**2
+S = a + b
+R = a - b
+P = a * b
+C = a / b
+console.log(S)
+console.log(R)
+console.log(P)
+console.log(C)
